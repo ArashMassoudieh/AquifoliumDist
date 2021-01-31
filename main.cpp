198a6e47f0acbdb731fa5d5dbfadacc9dc9f3e8c
Arash <arashmassoudieh@gmail.com>
2021-01-31 12:40:55 -0500

    Load and Run

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "System.h"
+#include "Script.h"
 using namespace std;
 
 int main()
 {
     System system;
-    system.GetQuanTemplate("/home/arash/Projects/QAquifolium/resources/main_components.json");
-    system.ReadSystemSettingsTemplate("/home/arash/Projects/QAquifolium/resources/settings.json");
-
+    //system.GetQuanTemplate("/home/arash/Projects/QAquifolium/resources/main_components.json");
+    //system.ReadSystemSettingsTemplate("/home/arash/Projects/QAquifolium/resources/settings.json");
+    Script scr("/home/arash/Dropbox/Aquifolium/infiltration_test.scr",&system);
+    system.clear();
+    system.CreateFromScript(scr);
+    system.Solve();
 }
